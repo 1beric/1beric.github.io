d8d096c39f37119e0c4b886b6be29cfddc07da93
Brandon Erickson <berickson2000@gmail.com>
2021-01-18 13:50:02 -0700

    fixed all of the project links

--- scripts/project-loader.js
+++ scripts/project-loader.js
@@ -1,7 +1,7 @@
 var projects = [
 	["./images/gallery/code.png", "https://github.com/1beric/c--compiler_Rust", "C-- Compiler"],
 	["./images/gallery/beryl.png", "https://github.com/1beric/BerylEngine", "Beryl Engine"],
-	["./images/gallery/milano-couch.png", "/projects/c--", "3D Modeling"],
+	// ["./images/gallery/milano-couch.png", "/projects/c--", "3D Modeling"],
 	["./images/gallery/placeholder.png", "https://github.com/1beric/insta", "Instagram Bot"],
 	["./images/gallery/placeholder.png", "https://github.com/1beric/mastermind", "Mastermind"],
 	["./images/gallery/placeholder.png", "https://osf.io/64m79/", "Nano2020"],
@@ -20,7 +20,7 @@ function loadProject(projectArr) {
 	var img = document.createElement("img");
 	img.src = projectArr[0];
 	img.addEventListener("click", () => {
-		window.location.pathname = projectArr[1];
+		window.location = projectArr[1];
 	});
 	div.appendChild(img);
 	var text = document.createElement("h2");
